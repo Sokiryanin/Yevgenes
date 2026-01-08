9867dc87861a7d3ad35f682c1a863674724715ca
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2026-01-08 13:37:35 +0100

    fix slider and buttons styles

--- src/components/layout/slider/slider.js
+++ src/components/layout/slider/slider.js
@@ -2,40 +2,45 @@ import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css/bundle';
 
-// Ініціалізація слайдерів
 function initSliders() {
   if (document.querySelector('.project__slider')) {
     new Swiper('.project__slider', {
       modules: [Navigation, Pagination],
+
       slidesPerView: 'auto',
       centeredSlides: true,
-      grabCursor: true,
-      spaceBetween: 8,
-      speed: 800,
       loop: true,
+      speed: 600,
+      spaceBetween: 8,
 
-      breakpoints: {
-        992: {
-          spaceBetween: 16
-        }
-      },
-      // Пагінація
+      // effect: 'coverflow',
+      // coverflowEffect: {
+      //   rotate: 30,
+      //   slideShadows: false
+      // },
 
       pagination: {
         el: '.project__bullets',
         clickable: true
       },
 
-      // Кнопки "вліво/вправо"
       navigation: {
         prevEl: '.project__arrow--left',
         nextEl: '.project__arrow--right'
       },
 
-      on: {}
+      breakpoints: {
+        768: {
+          spaceBetween: 24
+        },
+        992: {
+          spaceBetween: 24
+        }
+      }
     });
   }
 }
+
 document.querySelector('[data-fls-slider]')
   ? window.addEventListener('load', initSliders)
   : null;
