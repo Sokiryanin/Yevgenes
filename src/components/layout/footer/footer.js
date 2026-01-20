90a8dd82bf85f209700e0183619f1dec2b755771
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2026-01-20 17:31:26 +0100

    fix ?

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -1,1 +1,12 @@
-import './footer.scss'
+import './footer.scss';
+
+const span = document.querySelector('.footer__header-title span');
+
+if (span) {
+  const text = span.textContent;
+
+  if (text.endsWith('?')) {
+    span.innerHTML =
+      text.slice(0, -1) + '<span class="footer__question-mark">?</span>';
+  }
+}
