b01a756c0a80cb32e8d3dfcaf875f02e6835f164
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2026-03-01 10:18:21 +0100

    add sendmail test

--- src/php/sendmail/config.php
+++ src/php/sendmail/config.php
@@ -8,16 +8,19 @@ require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 
+// Загрузка конфигурации из .env
+$env = parse_ini_file(__DIR__ . '/../../../.env');
+
 $mail = new PHPMailer(true);
 $mail->CharSet = 'UTF-8';
 $mail->setLanguage('uk', 'phpmailer/language/');
 $mail->IsHTML(true);
 
 $mail->isSMTP(); // Send using SMTP
-$mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
+$mail->Host       = $env['SMTP_HOST']; // Set the SMTP server to send through
 $mail->SMTPAuth = true; // Enable SMTP authentication
-$mail->Username = 'eugene.sidorov.dp.ua@gmail.com'; // SMTP username (email)
-$mail->Password = 'etlg ujtm cnyc hcwd'; // SMTP password (Google Account -> Security -> App passwords -> Add)
+$mail->Username   = $env['SMTP_USER']; // SMTP username (email)
+$mail->Password   = $env['SMTP_PASS']; // SMTP password (Google Account -> Security -> App passwords -> Add)
 
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 $mail->Port = 465;
